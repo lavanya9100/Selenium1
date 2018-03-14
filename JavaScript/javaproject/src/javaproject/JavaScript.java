b171f49c6a2e4c9f953fe927e7a7accc90a115cb
lavanya9100 <lavanyanaveen55@gmail.com>
2018-03-14 17:22:41 +0530

    second comitt

---
+++ JavaScript/javaproject/src/javaproject/JavaScript.java
@@ -0,0 +1,14 @@
+package javaproject;
+
+public class JavaScript {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		int sum=0;
+		sum=a+b;
+		System.out.println(sum);
+
+	}
+
+}
